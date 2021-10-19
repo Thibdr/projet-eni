c16b0f9010efee5042a1656c746d80d10f25262d
HeijileBoss <kevin.faure.85110@gmail.com>
2021-10-19 15:51:55 +0200

    Modification du AppAuthenticator.php et début du form pour modifier l'utilisateur

--- eni-project/src/Controller/UtilisateurController.php
+++ eni-project/src/Controller/UtilisateurController.php
@@ -8,6 +8,7 @@ use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -16,7 +17,7 @@ class UtilisateurController extends AbstractController
     /**
      * @Route("/modificationUtilisateur", name="utilisateur")
      */
-    public function index(Request $request, ParticipantRepository $participantRepository): Response
+    public function index(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         if($this->getUser() != null) {
         $user = new Participant();
@@ -25,7 +26,7 @@ class UtilisateurController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            if($form->get('pseudo')->getData() != null){
+            if($form->get('pseudo')->getData() != null && $form->get('pseudo')->getData() != $user->getPseudo() ){
                 $user->setPseudo($form->get('pseudo')->getData());
             }
             if($form->get('prenom')->getData() != null){
@@ -40,9 +41,11 @@ class UtilisateurController extends AbstractController
             if($form->get('mail')->getData() != null){
                 $user->setMail($form->get('mail')->getData());
             }
-            /*if($form->get('password')->getData() != null){
-                $user->setPassword($form->get('password')->getData());
-            }*/
+            if($form->get('password')->getData() != null && $form->get('password')->getData() != $user->getPseudo())
+            $user->setPassword($userPasswordHasherInterface->hashPassword(
+                $user,
+                $form->get('password')->getData()
+            ));
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->flush();

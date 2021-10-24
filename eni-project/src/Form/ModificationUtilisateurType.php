e135bf4be673289eb0e014b10e6729bade2b6aba
HeijileBoss <kevin.faure.85110@gmail.com>
2021-10-24 19:46:11 +0200

    Ajout des contraintes

--- eni-project/src/Form/ModificationUtilisateurType.php
+++ eni-project/src/Form/ModificationUtilisateurType.php
@@ -14,6 +14,8 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Unique;
 
 class ModificationUtilisateurType extends AbstractType
@@ -21,11 +23,51 @@ class ModificationUtilisateurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
+            ->add('pseudo', TextType::class, [
+            'constraints' => [
+                new NotBlank(['message' => 'Veuillez renseigner un pseudo']),
+                new Length([
+                    'min' => 4,
+                    'max' => 20,
+                    'minMessage' => "Pseudo trop court ! Au moins 4 caractères !",
+                    'maxMessage' => "Pseudo trop long ! Au plus 20 caractères"
+                ])
+            ], 'label_attr' => [
+                    'class' => 'col-form-label'
+                ]
+            ])
             //->add('roles')
-            ->add('password', PasswordType::class)
-            ->add('nom')
-            ->add('prenom')
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez renseigner un mot de passe']),
+                ]
+            ])
+            ->add('nom', TextType::class, [
+        'constraints' => [
+            new NotBlank(['message' => 'Veuillez renseigner un pseudo']),
+            new Length([
+                'min' => 3,
+                'max' => 50,
+                'minMessage' => "Nom trop court ! Au moins 3 caractères !",
+                'maxMessage' => "Nom trop long ! Au plus 50 caractères"
+            ])
+        ], 'label_attr' => [
+            'class' => 'col-form-label'
+        ]
+    ])
+            ->add('prenom', TextType::class, [
+        'constraints' => [
+            new NotBlank(['message' => 'Veuillez renseigner un pseudo']),
+            new Length([
+                'min' => 3,
+                'max' => 50,
+                'minMessage' => "Prénom trop court ! Au moins 3 caractères !",
+                'maxMessage' => "Prénom trop long ! Au plus 50 caractères"
+            ])
+        ], 'label_attr' => [
+            'class' => 'col-form-label'
+        ]
+    ])
             ->add('telephone', TelType::class)
             ->add('mail', EmailType::class)
             ->add('photo', FileType::class)
